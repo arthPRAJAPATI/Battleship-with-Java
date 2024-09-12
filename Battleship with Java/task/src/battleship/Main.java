94bcfd8e3d6fe565a414347f5457941b9824ee30
ArthPrajapati2 <aprajapati2@conestogac.on.ca>
2024-09-12 15:04:22 -0400

    set the play field

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -3,46 +3,91 @@ package battleship;
 import java.util.Scanner;
 
 public class Main {
-
+    public static boolean HORIZONTAL = false;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[][] playField = new char[11][11];
+        String[][] playField = new String[11][11];
         playField = createPlayField(playField);
         printField(playField);
         System.out.println("Enter the coordinates of the ship:");
         String shipCoord = sc.nextLine();
         String[] coOrdinate = shipCoord.split(" ");
-        checkCoord(coOrdinate);
-        getNumericCoord(shipCoord);
+
+        boolean isCorrect =  checkCoord(coOrdinate);
+        if(isCorrect) {
+            int[] numericalCoord = getNumericCoord(coOrdinate);
+            getLengthAndParts(coOrdinate);
+        }
     }
 
-    public static void checkCoord(String[] shipCoord) {
+    public static void getLengthAndParts(String[] shipCoord) {
+        String end = shipCoord[1];
+        String cord = new String();
+        StringBuilder str = new StringBuilder();
+        char x = shipCoord[0].charAt(0);
+        int y =  Integer.parseInt(shipCoord[0].substring(1));
+        int len = 1;
+        if(HORIZONTAL) {
+            do {
+                str.append(x).append(y).append(" ");
+                if(y < Integer.parseInt(end.substring(1))){
+                    y++;
+                } else {
+                    y--;
+                }
+                len++;
+            }while(!end.substring(1).equals(String.valueOf(y)));
+        } else {
+            do{
+                str.append(x).append(y).append(" ");
+                if((int) x < (int) end.charAt(0)){
+                    x++;
+                } else {
+                    x--;
+                }
+                len++;
+            }while(x != end.charAt(0));
+
+        }
+        str.append(end);
+        System.out.println("length: " + len);
+        System.out.println("Parts: " + str.toString());
+    }
+
+    public static boolean checkCoord(String[] shipCoord) {
         if(shipCoord[0].charAt(0) != shipCoord[1].charAt(0) &&
-        shipCoord[0].substring(1) != shipCoord[1].substring(1)) {
+        shipCoord[0].charAt(1) != shipCoord[1].charAt(1)) {
             System.out.println("Error!");
+            return false;
         }
-        for(String s: shipCoord) {
-            if((int) s.charAt(0) < 48 || (int) s.charAt(0) > 57) {
+            if((int) shipCoord[0].charAt(0) < 65 || (int) shipCoord[1].charAt(0) > 74) {
                 System.out.println("Error!");
+                return false;
             }
-            if(Integer.parseInt(s.substring(1)) > 10) {
+            for(String s: shipCoord)
+            if(Integer.parseInt(s.substring(1)) > 10 || Integer.parseInt(s.substring(1)) < 1) {
                 System.out.println("Error!");
+                return false;
             }
-        }
+        return true;
     }
-    public static void getNumericCoord(String shipCoord) {
-        shipCoord = shipCoord.replace(" ","");
+    public static int[] getNumericCoord(String[] shipCoord) {
         int[] newCoord = new int[4];
-        for(int i = 0; i < shipCoord.length(); i++) {
-            int a = (int) shipCoord.charAt(i);
-            if(a < 48 || a > 57) {
-                newCoord[i] = a - 64;
+        for(int i = 0; i < shipCoord.length; i++) {
+            String x = shipCoord[i].substring(0, 1);
+            int y = Integer.parseInt(shipCoord[i].substring(1));
+            if(y < 1 || y > 10) {
+                newCoord[i] = y;
             } else {
-                newCoord[i] = a - 48;
+                newCoord[i] = (int) x.charAt(0);
             }
         }
+        if(shipCoord[0].charAt(0) == shipCoord[1].charAt(0)) {
+            HORIZONTAL = true;
+        }
+        return newCoord;
     }
-    public static void printField(char[][] playField){
+    public static void printField(String[][] playField){
         for(int i = 0; i < playField.length; i++){
             for (int j = 0; j < playField[0].length; j++) {
                 System.out.print(playField[i][j] + " ");
@@ -50,18 +95,18 @@ public class Main {
             System.out.println();
         }
     }
-    public static char[][] createPlayField(char[][] playField){
+    public static String[][] createPlayField(String[][] playField){
         int alpha = 65;
         for(int i = 0; i < playField.length; i++){
             for (int j = 0; j < playField[0].length; j++){
                 if(i == 0 && j == 0) {
-                    playField[i][j] = ' ';
+                    playField[i][j] = " ";
                 } else if(i == 0 && j != 0) {
-                    playField[i][j] = Character.forDigit(j, 10);
+                    playField[i][j] = String.valueOf(j);
                 } else if(j == 0 && i != 0) {
-                    playField[i][j] = (char) alpha++;
+                    playField[i][j] = String.valueOf((char)alpha++);
                 }else {
-                    playField[i][j] = '~';
+                    playField[i][j] = "~";
                 }
             }
         }
